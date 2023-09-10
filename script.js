a05f21bac90b34f03f8a08fa5c3b13de8cc66957
Okiki Areo <okikiareo@gmail.com>
2023-09-10 23:24:06 +0100

    js

--- script.js
+++ script.js
@@ -24,7 +24,35 @@ return {
     },
     inputString: function(){
         return classString;
+    },
+    
+// return items to the ui
+addItem: function(obj, type){
+    var html, newHtml, element;
+    if(type === "inc"){
+        element = classString.incomeList;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+    } else if(type === "exp"){
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        element = classString.expenseList;
     }
+    newHtml =  html.replace("%id%", obj.id);
+    newHtml = newHtml.replace("%description%", obj.description);
+    newHtml = newHtml.replace("%value%", obj.value);
+    
+    document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+    // clearing the field
+    clearField: function(){
+    var fields, fieldArr;
+    fields = document.querySelectorAll(classString.descript + "," + classString.addValue);
+    fieldArr = Array.prototype.slice.call(fields);
+    fieldArr.forEach(function(current, index, array){
+        current.value = "";
+    }) 
+    // Going back to the first field after input
+    fieldArr[0].focus();
+    },
 }) ();
 
 // Budget logic Controller
